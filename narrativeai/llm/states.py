ce9fbb88efd7da12ff1c754deb36d7fef6308daf
Jittabun <jj2jom@gmail.com>
2025-01-18 17:25:50 +0700

    implement longterm plotter(basically plot idea generator)

--- narrativeai/llm/states.py
+++ narrativeai/llm/states.py
@@ -1,10 +1,21 @@
 from typing import Annotated, Tuple, List, Literal
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
+from .models import StoryContext
 
 MessageRole = Literal["user", "assistant", "system"]
 Message = Tuple[MessageRole, str]
 
+def UpdateStoryContext(old: StoryContext, new: StoryContext):
+    """Update the story context with new values only if new values are provided."""
+    ctx = StoryContext(
+        genre=new.genre if new.genre else old.genre,
+        tone=new.tone if new.tone else old.tone
+    )
+    return ctx
+
 class GraphState(TypedDict):
     """State definition for the story generation workflow."""
-    messages: Annotated[List[Message], add_messages]
+    stories: Annotated[List[Message], add_messages]
+    longterm_plots: Annotated[List[str], add_messages]
+    context: Annotated[StoryContext, UpdateStoryContext]
